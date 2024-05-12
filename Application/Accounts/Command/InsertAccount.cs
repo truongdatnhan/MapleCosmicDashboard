bf2fa3cef1ab8c702402306d5f697f6e39e53f73
truongdatnhan <truongdatnhan@gmail.com>
2024-05-11 23:08:09 -0400

    register check (#4)

--- Application/Accounts/Command/InsertAccount.cs
+++ Application/Accounts/Command/InsertAccount.cs
@@ -9,15 +9,37 @@ using System.Threading.Tasks;
 
 namespace Application.Accounts.Command
 {
-    public record InsertAccountCommand(string Username, string Password, string Email) : IRequest<int>;
+    public record InsertAccountCommand(string Username, string Password, string Email) : IRequest<(bool IsSuccess, string? ErrorMessage)>;
 
-    public class InsertAccountHandler(ISqlDataAccess sqlDataAccess) : IRequestHandler<InsertAccountCommand, int>
+    public class InsertAccountHandler(ISqlDataAccess sqlDataAccess) : IRequestHandler<InsertAccountCommand, (bool IsSuccess, string? ErrorMessage)>
     {
-        public async Task<int> Handle(InsertAccountCommand request, CancellationToken cancellationToken)
+        public async Task<(bool IsSuccess, string? ErrorMessage)> Handle(InsertAccountCommand request, CancellationToken cancellationToken)
         {
             var passHashed = BCrypt.Net.BCrypt.HashPassword(request.Password, workFactor: 12);
-            return await sqlDataAccess.ExecuteAsync("INSERT INTO accounts (name, password, email) VALUES (@username, @password, @email) ",
+            string? error;
+
+            var usernameExist = (await sqlDataAccess.QueryFirstOrDefaultAsync<int>("SELECT COUNT(*) FROM Accounts WHERE Name = @username",
+                new { username = request.Username })) == 1;
+
+            if(usernameExist)
+            {
+                error = "Username already exists";
+                return (false, error);
+            }
+
+            var emailExist = (await sqlDataAccess.QueryFirstOrDefaultAsync<int>("SELECT COUNT(*) FROM Accounts WHERE Email = @email",
+                new { email = request.Email })) == 1;
+
+            if (emailExist)
+            {
+                error = "Email already exists";
+                return (false, error);
+            }
+
+            var result= await sqlDataAccess.ExecuteAsync("INSERT INTO accounts (name, password, email, gender) VALUES (@username, @password, @email, 0) ",
                 new { username = request.Username, password = passHashed, email = request.Email });
+
+            return (true, null);
         }
     }
 }
---
+++ Application/Dashboard/Query/CountPlayerOnline.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Common;
+using System.Security.Cryptography;
+
+namespace Application.Dashboard.Query
+{
+    public record CountPlayerOnlineQuery() : IRequest<int>;
+
+    public class CountPlayerOnlineHandler(ISqlDataAccess sqlDataAccess) : IRequestHandler<CountPlayerOnlineQuery, int>
+    {
+        public async Task<int> Handle(CountPlayerOnlineQuery request, CancellationToken cancellationToken)
+        {
+            return await sqlDataAccess.QueryFirstOrDefaultAsync<int>("SELECT COUNT(*) FROM Accounts WHERE loggedin = 2 ");
+        }
+
+    }
+}
