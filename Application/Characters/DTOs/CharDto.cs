aa860f51f5501234b8162db6393f00758a4f70c2
Đạt Nhân Trương <truongdatnhan@gmail.com>
2024-05-25 00:16:30 -0400

    creating dto and job enum

---
+++ Application/Characters/DTOs/CharDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Characters.DTOs
+{
+    public class CharDto
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<CharItemDto> ItemsEquipped { get; set; }
+
+        public CharDto(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            ItemsEquipped = new List<CharItemDto>();
+        }
+
+    }
+
+    public class CharItemDto
+    {
+        public int ItemId { get; set; }
+        public int ItemStatus { get; set; }
+    }
+
+}
