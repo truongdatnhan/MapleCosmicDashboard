b306b4b82b5b9499f17e476bb99a4882ce89c2d8
truongdatnhan <truongdatnhan@gmail.com>
2024-05-26 23:47:55 -0400

    Character render (#7)
    * creating dto and job enum
    
    * render character

---
+++ Application/Characters/DTOs/CharDto.cs
@@ -0,0 +1,86 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Application.Characters.DTOs
+{
+    public class CharDto
+    {
+        public int Rank { get; set; }
+        public int Level { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public Job Job { get; set; }
+        public bool IsFemale { get; set; }
+        public byte SkinColor { get; set; }
+        public int Hair { get; set; }
+        public int Face { get; set; }
+        public int Fame { get; set; }
+        public decimal Meso { get; set; }
+        public HashSet<CharItemDto> ItemsEquipped { get; set; } = new();
+        public string CharacterAvatar()
+        {
+            //add default if character not wearing anything
+            if (ItemsEquipped.Count == 0)
+            {
+                if (IsFemale)
+                {
+                    ItemsEquipped.Add(new(1041046));
+                    ItemsEquipped.Add(new(1061039));
+                }
+                else
+                {
+                    ItemsEquipped.Add(new(1042162));
+                    ItemsEquipped.Add(new(1062112));
+                }
+            }
+
+            //add body, head (crucial)
+            ItemsEquipped.Add(new(2000 + SkinColor));
+            ItemsEquipped.Add(new(12000 + SkinColor));
+
+            ItemsEquipped.Add(new(Hair));
+            ItemsEquipped.Add(new(Face));
+
+            string json = JsonSerializer.Serialize(ItemsEquipped, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            string imageUrl = $"https://maplestory.io/api/character/{json.Substring(1,json.Length-2)}/jump";
+            return imageUrl;
+        }
+
+        public string MesoFormatted()
+        {
+            char? letter;
+            decimal meso = 0;
+            if(Meso < 1000000) //1m
+            {
+                meso = Meso / 1000;
+                letter = 'K';
+            } else if(Meso < 100000000) //100m
+            {
+                meso = Meso / 1000000;
+                letter = 'M';
+            } else // 1b
+            {
+                meso = Meso / 1000000000;
+                letter = 'B';
+            }
+            return meso.ToString("#,0.#") + letter;
+        }
+    }
+
+    public class CharItemDto
+    {
+        public int ItemId { get; set; }
+        public string Region { set; get; } = "GMS";
+        public string Version { set; get; } = "83";
+
+        public CharItemDto(int itemId)
+        {
+            ItemId = itemId;
+        }
+    }
+
+}
---
+++ Application/Dashboard/Query/GetTopMesoPlayer.cs
@@ -0,0 +1,93 @@
+﻿using MediatR;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Common;
+using System.Security.Cryptography;
+using Application.Characters.DTOs;
+using System.Collections;
+using Dapper;
+
+namespace Application.Dashboard.Query
+{
+    public record GetTopMesoPlayerQuery() : IRequest<List<CharDto>>;
+
+    public class GetTopMesoPlayerHandler(ISqlDataAccess sqlDataAccess) : IRequestHandler<GetTopMesoPlayerQuery, List<CharDto>>
+    {
+        public async Task<List<CharDto>> Handle(GetTopMesoPlayerQuery request, CancellationToken cancellationToken)
+        {
+            var charDictionary = new Dictionary<int, CharDto>();
+
+            var query = @"WITH ranked_characters AS (
+                        SELECT
+                            c.id,
+                            c.name,
+                            c.level,
+                            c.job,
+                            c.gender,
+                            c.skincolor,
+                            c.hair,
+                            c.face,
+                            (c.meso + s.meso) AS total_meso,
+                            ROW_NUMBER() OVER (PARTITION BY acc.id ORDER BY c.id ASC, (c.meso + s.meso) DESC) AS rank_within_account
+                        FROM
+                            `accounts` acc
+                        JOIN `characters` c ON
+                            c.accountid = acc.id
+                        JOIN `storages` s ON
+                            acc.id = s.accountid
+                    ),
+                    top_characters AS (
+                        SELECT
+                            rc.*
+                        FROM
+                            ranked_characters rc
+                        WHERE
+                            rc.rank_within_account = 1
+                        LIMIT 5
+                    )
+                    SELECT
+                        DENSE_RANK() OVER (ORDER BY tc.id ASC,tc.total_meso DESC) AS rank,
+                        tc.name,
+                        tc.level,
+                        tc.job,
+                        tc.gender AS isFemale,
+                        tc.skincolor,
+                        tc.hair,
+                        tc.face,
+                        tc.total_meso AS meso,
+                        inv.itemid
+                    FROM
+                        top_characters tc
+                    LEFT JOIN `inventoryitems` inv ON
+                        inv.characterid = tc.id AND inv.inventorytype = -1;";
+
+            await sqlDataAccess.GetDbConnection().QueryAsync<CharDto, CharItemDto, CharDto>(
+                query,
+                (ch, item) =>
+                {
+                    CharDto? charEntry;
+
+                    if (!charDictionary.TryGetValue(ch.Rank, out charEntry))
+                    {
+                        charEntry = ch;
+                        charDictionary.Add(charEntry.Rank, charEntry);
+                    }
+
+                    if (item != null)
+                    {
+                        charEntry.ItemsEquipped.Add(item);
+                    }
+                    return ch;
+                },
+                splitOn: "Itemid"
+            );
+
+            return charDictionary.Values.ToList();
+        }
+
+    }
+}
