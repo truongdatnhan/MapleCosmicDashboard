b10694fc17d9f5ae3dfb07352724f910bc248e2a
Đạt Nhân Trương <truongdatnhan@gmail.com>
2025-03-04 12:31:49 -0500

    adding dockerfile and docker-compose

--- Infrastructure/ConfigureServices.cs
+++ Infrastructure/ConfigureServices.cs
@@ -15,7 +15,22 @@ namespace Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddTransient<ISqlDataAccess, SqlDataAccess>(_ => new SqlDataAccess(new MySqlConnection(configuration.GetConnectionString("Default"))));
+            services.AddTransient<ISqlDataAccess, SqlDataAccess>(_ => {
+                var connectionString = configuration.GetConnectionString("Default");
+
+                var dbDockerHost = configuration["DB_HOST"];
+                var dbDockerPort = configuration["DB_PORT"] ?? "3306";
+                var dbDockerDb = configuration["DB_DATABASE"];
+                var dbDockerUser = configuration["DB_USER"];
+                var dbDockerPass = configuration["DB_PASS"];
+
+                if (dbDockerHost != null && dbDockerDb != null && dbDockerUser != null && dbDockerPass != null)
+                {
+                    connectionString = @$"server={dbDockerHost}; port={dbDockerPort}; database={dbDockerDb}; user={dbDockerUser}; password={dbDockerPass}";
+                }
+
+                return new SqlDataAccess(new MySqlConnection(connectionString));
+            });
             return services;
         }
     }
